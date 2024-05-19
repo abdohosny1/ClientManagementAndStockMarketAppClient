07cf07f343ab99b6e2ea9080b542ab4b4479407c
abdohosny1 <make.y90@yahoo.com>
2024-05-19 09:45:00 +0300

    update environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production:false,
 BASE_URL:"https://localhost:7056/api",
 CLIENTS:"/Clients"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production:true,
+  BASE_URL:"http://clientmanagement-001-site1.htempurl.com/api",
+  CLIENTS:"/Clients"
+};
